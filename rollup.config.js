12d3f0b68842d6a0c7586fca018942e408e8cede
yumemi-kuwahara <k_kuwahara@yumemi.co.jp>
2018-04-19 12:53:59 +0900

    added: base config file of rollup

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import {rollup} from 'rollup'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import progress from 'rollup-plugin-progress'
+import commonjs from 'rollup-plugin-commonjs'
+import riot from 'rollup-plugin-riot'
+
+export default {
+   input: 'src/app.js',
+   output: {
+      file: 'public/js/app.js',
+      sourcemap: false,
+      format: 'iife',
+      strict: true
+   },
+   context: 'window',
+   plugins: [
+      riot(),
+      nodeREsolve({
+         jsnext: true,
+         main: true,
+         browser: true
+      }),
+      commonjs(),
+      buble()
+   ]
+}
